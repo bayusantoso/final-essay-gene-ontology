db8dd1b79d640ecabe6981cb3034a069b83e4574
Bayu Santoso <bayusantoso.mail@gmail.com>
2015-11-19 06:30:21 +0700

    Commit Changes
    - return parent, children, ancestor and offspring from a term

--- IMPLEMENTATION/4. Application/New/GOApps/app/views.py
+++ IMPLEMENTATION/4. Application/New/GOApps/app/views.py
@@ -14,15 +14,27 @@ def index():
 def result():
   form = SearchForm()
   query = form.query.data
-  relationship = form.relationship.data
   ontology = form.ontology.data
 
   state, dict_terms, messages = get_go_term(query)
 
-  result = []
+  result = {}
   message = []
-  if relationship == "children":
-    result, message = get_go_childrens(ontology, query)
+
+  child_result = []
+  parent_result = []
+  anccestor_result = []
+  offspring_result = []
+  if ontology != None and query != None and ontology != "" and query != "":
+    child_result, message = get_go_childrens(ontology, query)
+    parent_result, message = get_go_parents(ontology, query)
+    anccestor_result, message = get_go_ancesstors(ontology, query)
+    offspring_result, message = get_go_offsprings(ontology, query)
+
+  result["children"] = child_result
+  result["parent"] = parent_result
+  result["anccestor"] = anccestor_result
+  result["offspring"] = offspring_result
 
   return render_template('result.html', form=form, state = state, model = dict_terms, result = result, message = message)
 
@@ -32,10 +44,12 @@ def detail():
   state, dict_terms, message = get_go_term(id)
   return render_template('detail.html',id = id, state = state, result = dict_terms, message = message)
 
-def get_go_childrens(go_term_type, go_term):
+#private method
+#Get childrens of a term
+def get_go_childrens(go_term_ontology, go_term):
   robjects.r('''library(GO.db)''')
 
-  child_map = robjects.r["GO%sCHILDREN" % (go_term_type)]
+  child_map = robjects.r["GO%sCHILDREN" % (go_term_ontology)]
   children = []
 
   to_check = [go_term]
@@ -70,99 +84,124 @@ def get_go_childrens(go_term_type, go_term):
 
   return result, messages
 
-def get_go_parents(go_term_type, go_term):
-  robjects.r('''
-      library(GO.db)
-  ''')
+#Get parents of a term
+def get_go_parents(go_term_ontology, go_term):
+  robjects.r('''library(GO.db)''')
 
-  child_map = robjects.r["GO%sPARENTS" % (go_term_type)]
+  child_map = robjects.r["GO%sPARENTS" % (go_term_ontology)]
   children = []
 
   to_check = [go_term]
 
-  while len(to_check) > 0:
-        print(to_check)
-        new_children = []
-        for check_term in to_check:
-            if check_term != 'all':
-              if robjects.r.get(check_term, child_map):
-                if len(robjects.r.get(check_term, child_map)) > 0:
-                  new_children.extend(list(robjects.r.get(check_term, child_map)))
+  try:
+    while len(to_check) > 0:
+          new_children = []
+          for check_term in to_check:
+              if check_term != 'all':
+                if robjects.r.get(check_term, child_map):
+                  if len(robjects.r.get(check_term, child_map)) > 0:
+                    new_children.extend(list(robjects.r.get(check_term, child_map)))
 
-        if len(new_children) > 0:
           new_children = list(set([c for c in new_children if c]))
           children.extend(new_children)
-          to_check = new_children
-        else:
           to_check = []
+    children_state = True
+  except:
+    children_state = False
 
-  result = None
-  if len(children) > 0:
-    result = list(set(children))
-
-  return result
-
-def get_go_ancesstors(go_term_type, go_term):
-  robjects.r('''
-      library(GO.db)
-  ''')
-
-  child_map = robjects.r["GO%sANCESTOR" % (go_term_type)]
-  children = []
-
-  to_check = [go_term]
-  while len(to_check) > 0:
-        print(to_check)
-        new_children = []
-        for check_term in to_check:
-            if check_term != 'all':
-              if len(robjects.r.get(check_term, child_map)) > 0:
-                new_children.extend(list(robjects.r.get(check_term, child_map)))
-
-        if len(new_children) > 0:
-          new_children = list(set([c for c in new_children if c]))
-          children.extend(new_children)
-          to_check = new_children
+  result = []
+  messages = []
+  if children_state:
+    if len(children) > 0:
+      results = list(set(children))
+      for go_id in results:
+        state, dict_terms, message = get_go_term(go_id)
+        if state:
+          result.append(dict_terms)
         else:
-          to_check = []
+          messages.append(message)
 
-  result = None
-  if len(children) > 0:
-    result = list(set(children))
+  return result, messages
 
-  return result
+#Get ancesstors of a term
+def get_go_ancesstors(go_term_ontology, go_term):
+  robjects.r('''library(GO.db)''')
 
-def get_go_offsprings(go_term_type, go_term):
-  robjects.r('''
-      library(GO.db)
-  ''')
-
-  child_map = robjects.r["GO%sOFFSPRING" % (go_term_type)]
+  child_map = robjects.r["GO%sANCESTOR" % (go_term_ontology)]
   children = []
 
   to_check = [go_term]
 
-  while len(to_check) > 0:
-        print(to_check)
-        new_children = []
-        for check_term in to_check:
-            if check_term != 'all':
-              if len(robjects.r.get(check_term, child_map)) > 0:
-                new_children.extend(list(robjects.r.get(check_term, child_map)))
+  try:
+    while len(to_check) > 0:
+          new_children = []
+          for check_term in to_check:
+              if check_term != 'all':
+                if robjects.r.get(check_term, child_map):
+                  if len(robjects.r.get(check_term, child_map)) > 0:
+                    new_children.extend(list(robjects.r.get(check_term, child_map)))
 
-        if len(new_children) > 0:
           new_children = list(set([c for c in new_children if c]))
           children.extend(new_children)
-          to_check = new_children
-        else:
           to_check = []
+    children_state = True
+  except:
+    children_state = False
 
-  result = None
-  if len(children) > 0:
-    result = list(set(children))
+  result = []
+  messages = []
+  if children_state:
+    if len(children) > 0:
+      results = list(set(children))
+      for go_id in results:
+        state, dict_terms, message = get_go_term(go_id)
+        if state:
+          result.append(dict_terms)
+        else:
+          messages.append(message)
 
-  return result
+  return result, messages
 
+#Get offsprings of a term
+def get_go_offsprings(go_term_ontology, go_term):
+  robjects.r('''library(GO.db)''')
+
+  child_map = robjects.r["GO%sOFFSPRING" % (go_term_ontology)]
+  children = []
+
+  to_check = [go_term]
+
+  try:
+    while len(to_check) > 0:
+          new_children = []
+          for check_term in to_check:
+              if check_term != 'all':
+                if robjects.r.get(check_term, child_map):
+                  if len(robjects.r.get(check_term, child_map)) > 0:
+                    new_children.extend(list(robjects.r.get(check_term, child_map)))
+
+          new_children = list(set([c for c in new_children if c]))
+          children.extend(new_children)
+          to_check = []
+    children_state = True
+  except:
+    children_state = False
+
+  result = []
+  messages = []
+  if children_state:
+    if len(children) > 0:
+      results = list(set(children))
+      for go_id in results:
+        state, dict_terms, message = get_go_term(go_id)
+        if state:
+          result.append(dict_terms)
+        else:
+          messages.append(message)
+
+  return result, messages
+
+#Get details of a term
 def get_go_term(go_id):
   robjects.r('''library(GO.db)''')
   dict_terms = []
